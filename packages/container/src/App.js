940611a8ff427f648d202043c9f77b96d79953fa
EmmanuelBeja <beja.emmanuel@gmail.com>
2023-07-31 13:18:34 +0300

    bg(css): handle css conflict in header styling

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import MarkertingApp from "./components/MarkertingApp";
 import Header from "./components/Header";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "cont",
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarkertingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarkertingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
